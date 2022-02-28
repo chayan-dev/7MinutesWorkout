cfc7e7b23528574467ed6e9db1c18f76317ba050
chayan-dev <chayandas0308@gmail.com>
2022-03-01 00:48:27 +0530

    all done but history records are not showing

--- app/src/main/java/com/example/a7minuteworkout/HistoryActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/HistoryActivity.kt
@@ -2,8 +2,14 @@ package com.example.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a7minuteworkout.databinding.ActivityHistoryBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class HistoryActivity : AppCompatActivity() {
 
@@ -22,5 +28,39 @@ class HistoryActivity : AppCompatActivity() {
         binding?.toolbarHistoryActivity?.setNavigationOnClickListener{
             onBackPressed()
         }
+        val dao=(application as WorkoutApp).db.HistoryDao()
+        getAllCompletedDates(dao)
+    }
+
+    private fun getAllCompletedDates(historyDao: HistoryDao){
+        lifecycleScope.launch {
+            historyDao.fetchAllDates().collect { allCompletedDatesList->
+                if(allCompletedDatesList.isNotEmpty()){
+                    binding?.tvHistory?.visibility= View.VISIBLE
+                    binding?.rvHistory?.visibility=View.INVISIBLE
+                    binding?.tvNoDataAvailable?.visibility=View.INVISIBLE
+
+                    binding?.rvHistory?.layoutManager=LinearLayoutManager(this@HistoryActivity)
+
+                    val dates= ArrayList<String>()
+                    for (date in allCompletedDatesList){
+                        dates.add(date.date)
+                    }
+                    val historyAdapter=HistoryAdapter(dates)
+
+                    binding?.rvHistory?.adapter=historyAdapter
+                }
+                else{
+                    binding?.tvHistory?.visibility= View.GONE
+                    binding?.rvHistory?.visibility=View.GONE
+                    binding?.tvNoDataAvailable?.visibility=View.VISIBLE
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding=null
     }
 }
